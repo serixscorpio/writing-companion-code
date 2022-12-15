76fa2162ad24dd7be3ea4bb69b2256e5d7055665
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-12-15 15:36:20 +0800

    Update tests for python-mocks-where-to-patch
    Also add comment to explain the patch approach.

--- python-mocks-where-to-patch/test_clients.py
+++ python-mocks-where-to-patch/test_clients.py
@@ -1,12 +1,17 @@
+import api
 import client1
 import client2
 
 
 def test_client1_offset(mocker):
+    # `client1` already have a reference to `expensive_calculation`,
+    # so patch on `client1`.
     mock = mocker.patch.object(client1, "expensive_calculation", return_value=20)
     assert client1.offset(1) == 21
 
 
 def test_client2_offset(mocker):
-    mock = mocker.patch.object(client2.api, "expensive_calculation", return_value=20)
+    # `client2` does not have a reference to `expensive_calculation`,
+    # so patch on `api`.
+    mock = mocker.patch.object(api, "expensive_calculation", return_value=20)
     assert client2.offset(1) == 21
